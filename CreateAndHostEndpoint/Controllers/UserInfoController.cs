46b9e07272b9b6a34fe6674168dfd71d6fdb11ff
victoryosamede <uvictory0@gmail.com>
2023-09-12 10:27:13 +0100

    host endpoint

---
+++ CreateAndHostEndpoint/Controllers/UserInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreateAndHostEndpoint.Controllers
+{
+    [ApiController]
+    [Route("api/user-info")]
+    public class UserInfoController: ControllerBase
+    {
+
+        
+        [HttpGet]
+        public IActionResult GetInfo(string slackName, string track)
+        {
+            var userInfo = new UserInformation
+            {
+                SlackName = slackName,
+                DayOfWeek = DateTime.UtcNow.ToString("dddd"),
+                Track= track,
+
+
+            };
+
+        }
+    }
+}
---
+++ CreateAndHostEndpoint/UserInformation.cs
@@ -0,0 +1,15 @@
+﻿namespace CreateAndHostEndpoint
+{
+    public class UserInformation
+    {
+        public required string SlackName { get; set; }
+
+        public string DayOfWeek { get; set; } = DateTime.UtcNow.ToString("dddd");
+        public DateTime CurrentUtcTime { get; set; }
+        public required string Track { get; set; }
+        public string GithubFileUrl { get; set; } = string.Empty;
+        public string GithubRepoUrl { get; set;} = string.Empty;
+        public int StatusCode { get; set; }
+
+    }
+}
